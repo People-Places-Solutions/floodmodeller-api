668de51ddbac9c26d1963acbf6e620d780f0fa94
joe-pierce <joe_pierce_123@hotmail.com>
2022-06-13 17:38:53 +0100

    Align formatting with black

--- floodmodeller_api/validation/urban_parameters.py
+++ floodmodeller_api/validation/urban_parameters.py
@@ -15,57 +15,57 @@ address: Jacobs UK Limited, Flood Modeller, Cottons Centre, Cottons Lane, London
 """
 
 urban_parameter_options = {
-        'elevation': {
-        'type': 'type-match',
-        'options': (float, int)
+        "elevation": {
+        "type": "type-match",
+        "options": (float, int)
     },
-    'max_depth': {
-         'type': 'type-match',
-        'options': (float, int)
+    "max_depth": {
+         "type": "type-match",
+        "options": (float, int)
     },
-    'initial_depth': {
-        'type': 'type-match',
-        'options': (float, int)
+    "initial_depth": {
+        "type": "type-match",
+        "options": (float, int)
     },
-    'surface_depth': {
-        'type': 'type-match',
-        'options': (float, int)
+    "surface_depth": {
+        "type": "type-match",
+        "options": (float, int)
     },
-    'area_ponded': {
-        'type': 'type-match',
-        'options': (float, int)
+    "area_ponded": {
+        "type": "type-match",
+        "options": (float, int)
     },
-    'format': {
-         'type': 'value-match',
-        'options': ['INTENSITY', 'VOLUME', 'CUMULATIVE']
+    "format": {
+         "type": "value-match",
+        "options": ["INTENSITY", "VOLUME", "CUMULATIVE"]
     },
-    'interval': {    # TODO: UPDATE TO CONSIDER - decimal hours or hours:minutes format (e.g., 0:15 for 15-minute readings). search for presence of ';' during _read maybe?
+    "interval": {    # TODO: UPDATE TO CONSIDER - decimal hours or hours:minutes format (e.g., 0:15 for 15-minute readings). search for presence of ";" during _read maybe?
     #try turing to float else keep as string
-        'type': 'type-match',
-        'options': (float, int, str) # TODO: add new a type of match called RegEx match for example '[0-9]:[0-9]'
+        "type": "type-match",
+        "options": (float, int, str) # TODO: add new a type of match called RegEx match for example "[0-9]:[0-9]"
     },
-    'snow_catch_factor': {
-        'type': 'type-match',
-        'options': (float, int)
+    "snow_catch_factor": {
+        "type": "type-match",
+        "options": (float, int)
     },
-    'data_option': {
-         'type': 'value-match',
-        'options': ['TIMESERIES', 'FILE']
+    "data_option": {
+         "type": "value-match",
+        "options": ["TIMESERIES", "FILE"]
     },
-    'timeseries': {
-        'type': 'type-match',
-        'options': (str)
+    "timeseries": {
+        "type": "type-match",
+        "options": (str)
     },
-    'filename': {
-        'type': 'type-match',
-        'options': (str)
+    "filename": {
+        "type": "type-match",
+        "options": (str)
     },
-    'station': {
-        'type': 'type-match',
-        'options': (str)
+    "station": {
+        "type": "type-match",
+        "options": (str)
     },
-    'units': {
-         'type': 'value-match',
-        'options': ['IN', 'MM']
+    "units": {
+         "type": "value-match",
+        "options": ["IN", "MM"]
     }
 }
