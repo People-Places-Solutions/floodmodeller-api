f6311efb1dba774742c58509a66f21e33712205e
scleakey <s.c.leakey@gmail.com>
2022-08-04 09:48:51 +0100

    initial setup of lf1 file class

--- floodmodeller_api/__init__.py
+++ floodmodeller_api/__init__.py
@@ -6,5 +6,6 @@ from .ief import IEF
 from .ied import IED
 from .dat import DAT
 from .inp import INP
+from .lf1 import LF1
 
 from .util import open_docs
---
+++ floodmodeller_api/lf1.py
@@ -0,0 +1,119 @@
+"""
+Flood Modeller Python API
+Copyright (C) 2022 Jacobs U.K. Limited
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. 
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see https://www.gnu.org/licenses/.
+
+If you have any query about this program or this License, please contact us at support@floodmodeller.com or write to the following 
+address: Jacobs UK Limited, Flood Modeller, Cottons Centre, Cottons Lane, London, SE1 2QG, United Kingdom.
+"""
+
+from pathlib import Path
+from typing import Optional, Union
+from unicodedata import category
+
+from ._base import FMFile
+
+class LF1(FMFile):
+    """Reads and processes Flood Modeller 1D log file '.lf1'
+
+    Args:
+        lf1_filepath (str): Full filepath to model lf1 file
+
+    Output:
+        Initiates 'LF1' class object
+    """
+
+    _filetype: str = "LF1"
+    _suffix: str = ".lf1"
+
+    _ultimate_prefix = "!!"
+    _prefixes = [
+            "Info1 Timestep",
+            "Info1 Elapsed",
+            "Info1 Simulated",
+            "Info1 EFT:",
+            "Info1 ETR:",
+            "PlotI1",
+            "PlotC1",
+            "PlotF1"
+        ]
+
+    def __init__(self, lf1_filepath: Optional[Union[str, Path]]):
+        try:
+            self._filepath = lf1_filepath
+            FMFile.__init__(self)
+
+            # counter to keep track of file during simulation
+            self._lines_read = 0 
+
+            # dictionary to hold sorted lines according to prefix
+            self._prefixes_dict = {prefix: [] for prefix in self._prefixes}
+
+            self._read()
+
+        except Exception as e:
+            self._handle_exception(e, when="read")
+
+    def _read(self, force_reread = False):
+        # Read LF1 file
+        with open(self._filepath, "r") as lf1_file:
+            self._raw_data = [line.rstrip("\n") for line in lf1_file.readlines()]
+
+        # Force rereading from start of file
+        if force_reread == True:
+            self._lines_read = 0
+
+        # Process file
+        self._sort_lines()
+
+    def _sort_lines(self):
+        """Sorts raw data into lists for each prefix"""
+
+        self._print_lines_read()
+
+        # loop through lines that haven't already been read
+        raw_lines = self._raw_data[self._lines_read:]
+
+        for raw_line in raw_lines:
+            
+            # categorise lines according to prefix
+            for prefix in self._prefixes_dict:
+
+                # list to append line to
+                sorted_lines = self._prefixes_dict[prefix]
+
+                # lines which start with prefix
+                start_of_line = self._ultimate_prefix + prefix
+
+                # add everything after the prefix to its list
+                if raw_line.startswith(start_of_line):
+                    sorted_lines.append(raw_line.split(start_of_line)[1].lstrip())
+            
+            # update counter
+            self._lines_read += 1
+
+        self._print_lines_read()
+
+    def _print_lines_read(self):
+        """Prints the number of lines that have been read so far"""
+
+        print("Lines read: " + str(self._lines_read))
+
+    def _get_elapsed_time(self):
+        pass
+
+    def _get_simulated_time(self):
+        pass
+
+    def _get_EFT(self):
+        pass
+
+    def _get_ETR(self):
+        pass
