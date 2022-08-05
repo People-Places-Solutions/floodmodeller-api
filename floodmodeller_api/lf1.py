12a2b6c4e88991dde7cf1ee9b46b85c36cdddeec
scleakey <s.c.leakey@gmail.com>
2022-08-05 14:37:56 +0100

    change from if statements to classes

--- floodmodeller_api/lf1.py
+++ floodmodeller_api/lf1.py
@@ -37,38 +37,50 @@ class LF1(FMFile):
     _filetype: str = "LF1"
     _suffix: str = ".lf1"
 
-    _ultimate_prefix = "!!"
-    _prefixes_dict = {
-            # start
-            "Version": "Info1 version1d",
-            "Number of nodes": "output1  Number of 1D river nodes in model:",
-            # running
-            "Timestep": "Info1 Timestep",
-            "Elapsed time": "Info1 Elapsed",
-            "Simulated time": "Info1 Simulated",
-            "Estimated finish time": "Info1 EFT:",
-            "Estimated time remaining": "Info1 ETR:",
-            "Iterations": "PlotI1",
-            "Convergence": "PlotC1",
-            "Flow": "PlotF1",
-            "Mass error": "Info1 Mass %error =",
-            "Progress": "Progress1",
-            # end
-            "Initial volume": "output1  Initial volume:",
-            "Final volume": "output1  Final volume:",
-        }
-
     def __init__(self, lf1_filepath: Optional[Union[str, Path]]):
         try:
             self._filepath = lf1_filepath
             FMFile.__init__(self)
 
-            # counter to keep track of file during simulation
-            self._lines_read = 0 
+            # to keep track of file during simulation
+            self._lines_read = 0 # number of lines that have been read so far
+            self._stage_of_sim = 0 # 1 = start, 2 = running, 3 = end
 
-            # dictionary to hold processed data according to type
-            self._processed_lines_dict = {key: [] for key in self._prefixes_dict.keys()}
+            # to process and hold data according to type         
+            self.version = String("!!Info1 version1d")
+            self.number_of_nodes = Int("!!output1  Number of 1D river nodes in model:")
 
+            self.timestep = Float("!!Info1 Timestep")
+            self.elapsed_time = TimeDelta("!!Info1 Elapsed")
+            self.simulated_time = TimeDelta("!!Info1 Simulated")
+            self.estimated_finish_time = Time("!!Info1 EFT:")
+            self.estimated_time_remaining = TimeDelta("!!Info1 ETR:")
+            self.iterations = FloatMult("!!PlotI1")
+            self.convergence = FloatMult("!!PlotC1")
+            self.flow = FloatMult("!!PlotF1")
+
+            self.mass_error = FloatMult("!!Info1 Mass %error =")
+            self.progress = IntSplit("!!Progress1")
+            self.initial_volume = FloatSplit("!!output1  Initial volume:")
+            self.final_volume = FloatSplit("!!output1  Final volume:")
+
+            self._data_to_extract = [
+                self.version,
+                self.number_of_nodes,
+                self.timestep,
+                self.elapsed_time,
+                self.simulated_time,
+                self.estimated_finish_time,
+                self.estimated_time_remaining,
+                self.iterations,
+                self.convergence,
+                self.flow,
+                self.mass_error,
+                self.progress,
+                self.initial_volume,
+                self.final_volume
+            ]
+            
             self._read()
 
         except Exception as e:
@@ -94,20 +106,25 @@ class LF1(FMFile):
         # loop through lines that haven't already been read
         raw_lines = self._raw_data[self._lines_read:]
         for raw_line in raw_lines:
-            
-            # categorise lines according to prefix
-            for key in self._prefixes_dict.keys():
+
+            # what stage of simulation we are in (TODO: check robust)
+            if self._stage_of_sim == 0 and raw_line == "!!output1":
+                self._stage_of_sim = 1 #start
+            elif self._stage_of_sim == 1 and raw_line == "!!Progress1   0%":
+                self._stage_of_sim = 2 #running
+            elif self._stage_of_sim == 2 and raw_line == "!!output1":
+                self._stage_of_sim = 3 #end
+
+            # loop through types of lines
+            for line_type in self._data_to_extract:
 
                 # lines which start with prefix
-                start_of_line = self._ultimate_prefix + self._prefixes_dict[key]
+                start_of_line = line_type._prefix
                 if raw_line.startswith(start_of_line):
-
-                    # list to append to
-                    processed_lines = self._processed_lines_dict[key]
 
                     # add everything after prefix to list
                     end_of_line = raw_line.split(start_of_line)[1].lstrip()
-                    processed_lines.append(self._str_to_data(end_of_line, key))
+                    line_type._store_line(end_of_line)
             
             # update counter
             self._lines_read += 1
@@ -119,75 +136,109 @@ class LF1(FMFile):
 
         print("Lines read: " + str(self._lines_read))
 
-    def _str_to_data(self, data_str, key):
-        """Processes string into meaningful data"""
+class LineType:
 
-        # one float
-        if key == "Timestep":
-            processed_data = float(data_str)
+    def __init__(self, prefix):
+        self._prefix = prefix
+        self._init_value()
 
-        # one integer
-        elif key == "Number of nodes":
-            processed_data = int(data_str)
+    def _init_value(self):
+        self.value = []
 
-        # multiple floats
-        elif key in ("Iterations", "Convergence", "Flow", "Mass error"):
-            processed_data = [float(x) for x in data_str.split()]
+    def _store_line(self, raw_line):
+        processed_line = self._process_line(raw_line)
+        self.value.append(processed_line)
 
-        # volume (m3)
-        elif key in ("Initial volume", "Final volume"):
-            processed_data = float(data_str.split("m3")[0])
+    def _process_line(self):
+        raise NotImplementedError
 
-        # percentage (%)
-        elif key == "Progress":
-            processed_data = float(data_str.split("%")[0])/100
+class Time(LineType):
 
-        # time
-        elif key == "Estimated finish time":
-            processed_data = self._str_to_time(data_str)
-
-        # timedelta
-        elif key in ("Elapsed time", "Simulated time", "Estimated time remaining"):
-            processed_data = self._str_to_timedelta(data_str)
-
-        # string
-        elif key == "Version":
-            processed_data = data_str
-
-        else:
-            raise ValueError(f"{key} is not a valid prefix")
-
-        return(processed_data)
-
-    def _str_to_time(self, data_str):
+    def _process_line(self, raw):
         """Converts string HH:MM:SS to time"""
         
         try:
-            data_time = dt.datetime.strptime(data_str, "%H:%M:%S").time()
+            processed = dt.datetime.strptime(raw, "%H:%M:%S").time()
 
         except ValueError as e:
-            if data_str == "calculating...": #at start of simulation
-                data_time = pd.NaT
+            if raw == "calculating...": #at start of simulation
+                processed = pd.NaT
             else:
                 raise e
         
-        return(data_time)
+        return(processed)
 
-    def _str_to_timedelta(self, data_str):
+class TimeDelta(LineType):
+
+    def _process_line(self, raw):
         """Converts string HH:MM:SS to timedelta"""
 
         try:
-            h,m,s = data_str.split(":")
-            data_timedelta = dt.timedelta(
+            h,m,s = raw.split(":")
+            processed = dt.timedelta(
                 hours = int(h),
                 minutes = int(m),
                 seconds = int(s)
                 )
 
         except ValueError as e:
-            if data_str == "...": #at start of simulation
-                data_timedelta = pd.NaT
+            if raw == "...": #at start of simulation
+                processed = pd.NaT
             else:
                 raise e
 
-        return(data_timedelta)
+        return(processed)
+
+class Float(LineType):
+
+    def _process_line(self, raw):
+        """Converts string to float"""
+
+        processed = float(raw)
+
+        return(processed)
+
+class Int(LineType):
+
+    def _process_line(self, raw):
+        """Converts string to integer"""
+
+        processed = int(raw)
+
+        return(processed)
+
+class FloatSplit(LineType):
+
+    def _process_line(self, raw):
+        """Converts string to float, removing m3 units"""
+
+        processed = float(raw.split("m3")[0])
+
+        return(processed)
+
+class IntSplit(LineType):
+
+    def _process_line(self, raw):
+        """Converts string to integer, removing % sign"""
+
+        processed = int(raw.split("%")[0])
+
+        return(processed)
+
+class String(LineType):
+
+    def _process_line(self, raw):
+        """No conversion necessary"""
+
+        processed = raw
+
+        return(processed)
+
+class FloatMult(LineType):
+
+    def _process_line(self, raw):
+        """Converts string to list of floats"""
+
+        processed = [float(x) for x in raw.split()]
+
+        return(processed)
