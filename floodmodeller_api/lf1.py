441fa1734109ea9ec16e2ecf67f5fc59b8f4c7d6
scleakey <s.c.leakey@gmail.com>
2022-08-08 10:04:07 +0100

    timestep defines number of iterations

--- floodmodeller_api/lf1.py
+++ floodmodeller_api/lf1.py
@@ -44,8 +44,9 @@ class LF1(FMFile):
             FMFile.__init__(self)
 
             # to keep track of file during simulation
-            self._lines_read = 0 # number of lines that have been read so far
-            self._sim_stage = "init" # init, start, run, end
+            self._no_lines = 0 #number of lines that have been read so far
+            self._no_iters = 0 #number of iterations so far
+            self._sim_stage = "init" #init, start, run, end
 
             # to process and hold data according to type 
             stage = "start"    
@@ -54,7 +55,7 @@ class LF1(FMFile):
 
             stage = "run"
             self.progress = IntSplit("!!Progress1", stage = stage, split = "%")
-            self.timestep = Float("!!Info1 Timestep", stage = stage)
+            self.timestep = Float("!!Info1 Timestep", stage = stage, defines_iters = True)
             self.elapsed_time = TimeDelta("!!Info1 Elapsed", stage = stage)
             self.simulated_time = TimeDelta("!!Info1 Simulated", stage = stage)
             self.estimated_finish_time = Time("!!Info1 EFT:", stage = stage)
@@ -128,8 +129,9 @@ class LF1(FMFile):
 
         # Force rereading from start of file
         if force_reread == True:
-            # Reset counter
-            self._lines_read = 0
+            # Reset counters
+            self._no_lines = 0
+            self._no_iters = 0
 
             # Wipe values
             for line_type in self._data_to_extract:
@@ -141,10 +143,10 @@ class LF1(FMFile):
     def _process_lines(self):
         """Sorts and processes raw data into lists for each prefix"""
 
-        self._print_lines_read()
+        self._print_no_lines()
 
         # loop through lines that haven't already been read
-        raw_lines = self._raw_data[self._lines_read:]
+        raw_lines = self._raw_data[self._no_lines:]
         for raw_line in raw_lines:
 
             self._update_sim_stage(raw_line)
@@ -158,32 +160,33 @@ class LF1(FMFile):
                     # store everything after prefix
                     end_of_line = raw_line.split(line_type._prefix)[1].lstrip()
                     line_type._store(end_of_line)
+                    self._no_iters = line_type._update_iters(self._no_iters)
             
             # update counter
-            self._lines_read += 1
+            self._no_lines += 1
 
-        self._print_lines_read()
+        self._print_no_lines()
 
-    def _print_lines_read(self):
+    def _print_no_lines(self):
         """Prints the number of lines that have been read so far"""
 
-        print("Lines read: " + str(self._lines_read))
+        print("Lines read: " + str(self._no_lines))
 
-    def _update_sim_stage(self, raw_line):
+    def _update_sim_stage(self, raw):
         """Update what stage of simulation we are in"""
 
         # TODO: check classification is robust
 
         # start
-        if self._sim_stage == "init" and raw_line == "!!output1":
+        if self._sim_stage == "init" and raw == "!!output1":
             self._sim_stage = "start"
 
         # running
-        elif self._sim_stage == "start" and raw_line == "!!Progress1   0%":
+        elif self._sim_stage == "start" and raw== "!!Progress1   0%":
             self._sim_stage = "run"
 
         # end
-        elif self._sim_stage == "run" and raw_line == "!!output1":
+        elif self._sim_stage == "run" and raw == "!!output1":
             self._sim_stage = "end"
 
         elif self._sim_stage not in ("init", "start", "run", "end"):
@@ -192,14 +195,19 @@ class LF1(FMFile):
 class LineType(ABC):
     """Abstract base class for processing and storing different types of line"""
 
-    def __init__(self, prefix, stage):
+    def __init__(self, prefix, stage, defines_iters = False):
         self._prefix = prefix
 
+        if defines_iters == True:
+            self._update_iters = self._increment
+        else:
+            self._update_iters = self._do_not_increment
+
         if stage == "run":
-            self.value = []
+            self.value = [] #list
             self._store = self._append
         elif stage in ("start", "end"):
-            self.value = None
+            self.value = None #single value
             self._store = self._replace
         else:
             raise ValueError(f"Unexpected simulation stage \"{stage}\"")
@@ -214,6 +222,15 @@ class LineType(ABC):
     def _replace(self, raw_line):
         processed_line = self._process_line(raw_line)
         self.value = processed_line
+
+    @staticmethod
+    def _increment(iters):
+        iters += 1
+        return iters
+
+    @staticmethod
+    def _do_not_increment(iters):
+        return iters
 
     @abstractmethod
     def _process_line(self):
@@ -276,8 +293,8 @@ class Int(LineType):
 
 class FloatSplit(LineType):
 
-    def __init__(self, prefix, stage, split):
-        super().__init__(prefix, stage)
+    def __init__(self, prefix, stage, split, defines_iters = False):
+        super().__init__(prefix, stage, defines_iters)
         self._split = split
 
     def _process_line(self, raw):
@@ -289,8 +306,8 @@ class FloatSplit(LineType):
 
 class IntSplit(LineType):
 
-    def __init__(self, prefix, stage, split):
-        super().__init__(prefix, stage)
+    def __init__(self, prefix, stage, split, defines_iters = False):
+        super().__init__(prefix, stage, defines_iters)
         self._split = split
 
     def _process_line(self, raw):
