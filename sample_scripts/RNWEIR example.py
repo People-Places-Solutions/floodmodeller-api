32c590c7ed876b81d34eb7d6f19c717841e2c077
joe-pierce <joe_pierce_123@hotmail.com>
2022-09-09 16:46:55 +0100

    clean up rnweir sample script

--- sample_scripts/RNWEIR example.py
+++ sample_scripts/RNWEIR example.py
@@ -1,12 +1,22 @@
 #Import modules
-from floodmodeller_api import DAT
+import sys
+import os
+from pathlib import Path
+try:
+    from floodmodeller_api import DAT
+except ImportError:
+    print('Import failed - Please ensure you have correctly installed floodmodeller_api to your active environment')
+    sys.exit()
+
+script_loc = Path(__file__).resolve().parent
+os.chdir(script_loc) # Set current working directory to this script location
 
 #Initialise DAT class
-dat = DAT("C:/Users/CHRISTA4/OneDrive - Jacobs/Documents/FM API/floodmodeller-api/test/test_data/ex4.DAT")
+dat = DAT("sample_data/ex4.DAT")
 
 #Iterate through all round nosed weir sections
 for name, structure in dat.structures.items():
     if(structure._unit == "RNWEIR"):
         structure.upstream_crest_height += 0.3 #Increase upstream crest height by 0.3m
 
-dat.save("C:/Users/CHRISTA4/OneDrive - Jacobs/Documents/FM API/floodmodeller-api/test/test_data/ex4_changed.DAT")
+dat.save("sample_data/ex4_changed.DAT")
