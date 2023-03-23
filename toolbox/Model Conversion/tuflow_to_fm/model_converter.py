b9493df57fab710e1e31eee8adac565bf0dd963e
scleakey <s.c.leakey@gmail.com>
2023-03-23 13:23:49 +0000

    another rearrange of component converters

--- toolbox/Model Conversion/tuflow_to_fm/model_converter.py
+++ toolbox/Model Conversion/tuflow_to_fm/model_converter.py
@@ -18,22 +18,7 @@ class ModelConverter:
             level=logging.INFO,
         )
         self._logger = logging.getLogger("model_converter")
-        self._logger.info("initialising conversion process")
-
-        self._component_converters = {}
-
-    def convert(self):
-        self._logger.info("starting conversion process")
-
-        for k, v in self._component_converters.items():
-            self._logger.info(f"converting {k}")
-            try:
-                v.convert()
-                self._logger.info("success")
-            except Exception:
-                # self._logger.error("failure")
-                # self._logger.debug("", exc_info=True)
-                self._logger.exception("failure")
+        self._logger.info("starting conversion")
 
 
 class ModelConverter2D(ModelConverter):
@@ -76,46 +61,59 @@ class TuflowModelConverter2D(ModelConverter2D):
             path = self._tcf.get_path(v)
             setattr(self, f"_{k}", TuflowParser(path))
 
-        self._stage_type = "computational area"
-        self._init_one_component_converter(
-            [self._init_loc_line_converter],
-            ["loc_line"],
+        self._convert_one_component(
+            "computational area",
+            ["loc line"],
+            [self._create_loc_line_converter],
             [self.LOC_LINE_NAMES],
             [self._tgc],
         )
 
-    def _init_one_component_converter(
+    def _convert_one_component(
         self,
+        cc_type: str,
+        cc_subtypes: list[str],
         cc_inits: list[callable],
-        cc_names: list[str],
         cc_dicts: list[dict],
         cc_parsers: list[TuflowParser],
     ):
-        self._logger.info(f"initialising {self._stage_type}")
+        self._logger.info(f"converting {cc_type}")
 
-        for cc_init, cc_name, cc_dict, cc_parser in zip(
-            cc_inits, cc_names, cc_dicts, cc_parsers
+        for cc_init, cc_subtype, cc_dict, cc_parser in zip(
+            cc_inits, cc_subtypes, cc_dicts, cc_parsers
         ):
-            self._logger.info(f"checking for {cc_name} settings")
+            self._logger.info(f"checking for {cc_subtype} settings")
 
             if not cc_parser.check_names(cc_dict.values()):
-                self._logger.info(f"{cc_name} settings not detected")
+                self._logger.info(f"{cc_subtype} settings not detected")
                 continue
 
-            self._logger.info(f"{cc_name} settings detected")
+            self._logger.info(f"{cc_subtype} settings detected")
 
             try:
-                cc_init(cc_dict, cc_parser)
+                cc = cc_init(cc_dict, cc_parser)
             except:
-                # self._logger.error("{cc_name} settings not valid")
+                # self._logger.error("{cc_subtype} settings not valid")
                 # self._logger.debug("", exc_info=True)
-                self._logger.exception(f"{cc_name} settings not valid")
+                self._logger.exception(f"{cc_subtype} settings not valid")
                 continue
 
-            self._logger.info(f"{cc_name} settings valid")
+            self._logger.info(f"{cc_subtype} settings valid")
+
+            self._logger.info(f"converting {cc_subtype}")
+
+            try:
+                cc.convert()
+                self._logger.info("success")
+            except Exception:
+                # self._logger.error("failure")
+                # self._logger.debug("", exc_info=True)
+                self._logger.exception("failure")
+                continue
+
             break
 
-    def _init_loc_line_converter(self, cc_dict: dict, cc_parser: TuflowParser):
+    def _create_loc_line_converter(self, cc_dict: dict, cc_parser: TuflowParser):
 
         xml = self._xml
         inputs_folder = self._inputs_folder
@@ -130,7 +128,7 @@ class TuflowModelConverter2D(ModelConverter2D):
         active_area = all_areas[all_areas["code"] == 1].drop(columns="code")
         deactive_area = all_areas[all_areas["code"] == 0].drop(columns="code")
 
-        self._component_converters[self._stage_type] = LocLineConverter(
+        cc = LocLineConverter(
             xml=xml,
             inputs_folder=inputs_folder,
             domain_name=domain_name,
@@ -141,3 +139,5 @@ class TuflowModelConverter2D(ModelConverter2D):
             active_area=active_area,
             deactive_area=deactive_area,
         )
+
+        return cc
