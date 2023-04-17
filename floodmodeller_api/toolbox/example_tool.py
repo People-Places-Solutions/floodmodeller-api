f0b0d7a2cf619bc13ed6df281df7c174a645b2e6
scleakey <s.c.leakey@gmail.com>
2023-04-17 15:37:24 +0100

    renaming and reorganising

--- floodmodeller_api/toolbox/example_tool.py
+++ floodmodeller_api/toolbox/example_tool.py
@@ -2,15 +2,16 @@
 from floodmodeller_api.tool import FMTool, Parameter
 
 # ------ Define function ----- #
-# This is the funciton that should be run by the tool. In  this case it is just a simple sum function but you can
-# substitute any function in here
-def my_sum(a : float, b : float):
+# This is the funciton that should be run by the tool.
+# In this case it is just a simple sum function but you can
+# substitute any function in here.
+def my_sum(a: float, b: float):
     ab = a + b
     print(ab)
     return ab
 
+
 # ------ Create the Tool ------ #
-# 
 class SumTool(FMTool):
     # Define the name (required):
     name = "Sum tool"
@@ -19,11 +20,25 @@ class SumTool(FMTool):
     # Add the parameters (one per function argument):
     parameters = [
         # Add parameters using the Parameter class
-        Parameter(name = "a", dtype = float, description="the first number", help_text="", required=True),
-        Parameter(name = "b", dtype = float, description="the second number", help_text="", required=True)
+        Parameter(
+            name="a",
+            dtype=float,
+            description="the first number",
+            help_text="",
+            required=True,
+        ),
+        Parameter(
+            name="b",
+            dtype=float,
+            description="the second number",
+            help_text="",
+            required=True,
+        ),
     ]
     # Add the function to run
     tool_function = my_sum
 
-tool = SumTool()
-tool.run_gui()
+
+if __name__ == "__main__":
+    tool = SumTool()
+    tool.run_from_command_line()
