4ff8215acd2fccf234fba4577a5920af55d9cc89
scleakey <s.c.leakey@gmail.com>
2023-04-17 10:56:11 +0100

    remove unnecessary conversion

--- toolbox/ModelConversion/tuflow/test_file_parser.py
+++ toolbox/ModelConversion/tuflow/test_file_parser.py
@@ -93,20 +93,18 @@ def test_path(tuflow_parser, path1, path2):
 
 
 def test_geodataframe(tuflow_parser, mocker, path2):
-    empty_gdf = gpd.GeoDataFrame()
-    read_gdf = mocker.patch("geopandas.read_file", return_value=empty_gdf)
-    assert tuflow_parser.get_geodataframe("var1").equals(empty_gdf)
-    assert read_gdf.call_count == 1
-    assert str(read_gdf.call_args_list[0][0][0]) == str(path2)
+    read_file = mocker.patch("geopandas.read_file", return_value="test")
+    assert tuflow_parser.get_geodataframe("var1") == "test"
+    assert read_file.call_count == 1
+    assert read_file.call_args_list[0][0][0] == path2
 
 
 def test_dataframe(tuflow_parser, mocker, path2):
-    empty_df = pd.DataFrame()
-    read_df = mocker.patch("pandas.read_csv", return_value=empty_df)
-    assert tuflow_parser.get_dataframe("var1").equals(empty_df)
-    assert read_df.call_count == 1
-    assert str(read_df.call_args_list[0][0][0]) == str(path2)
-    assert str(read_df.call_args_list[0][1]["comment"]) == "!"
+    read_csv = mocker.patch("pandas.read_csv", return_value="test")
+    assert tuflow_parser.get_dataframe("var1") == "test"
+    assert read_csv.call_count == 1
+    assert read_csv.call_args_list[0][0][0] == path2
+    assert read_csv.call_args_list[0][1]["comment"] == "!"
 
 
 def test_single_geometry(tuflow_parser, mocker, path1, path2):
@@ -118,11 +116,11 @@ def test_single_geometry(tuflow_parser, mocker, path1, path2):
 
     assert tuflow_parser.get_single_geometry("var1") == point1
     assert read_gdf.call_count == 1
-    assert str(read_gdf.call_args_list[0][0][0]) == str(path2)
+    assert read_gdf.call_args_list[0][0][0] == path2
 
     assert tuflow_parser.get_single_geometry("var1", index=0, geom_index=1) == point2
     assert read_gdf.call_count == 2
-    assert str(read_gdf.call_args_list[1][0][0]) == str(path1)
+    assert read_gdf.call_args_list[1][0][0] == path1
 
 
 def test_all_paths(tuflow_parser, path1, path2):
@@ -130,24 +128,11 @@ def test_all_paths(tuflow_parser, path1, path2):
 
 
 def test_all_geodataframes(tuflow_parser, mocker, path3, path4, path5, path6):
-    
-    empty_gdf = gpd.GeoDataFrame()
-    read_gdf = mocker.patch("geopandas.read_file", return_value=empty_gdf)
+    read_gdf = mocker.patch("geopandas.read_file", return_value="test")
     result = tuflow_parser.get_all_geodataframes("var2")
-
-    assert type(result) == list
-    assert len(result) == 2
-    assert type(result[0]) == tuple
-    assert len(result[0]) == 2
-    assert type(result[1]) == tuple
-    assert len(result[1]) == 2
-
-    assert result[0][0].equals(empty_gdf)
-    assert result[0][1].equals(empty_gdf)
-    assert result[1][0].equals(empty_gdf)
-    assert result[1][1].equals(empty_gdf)
+    assert result == [("test", "test"), ("test", "test")]
     assert read_gdf.call_count == 4
-    assert str(read_gdf.call_args_list[0][0][0]) == str(path3)
-    assert str(read_gdf.call_args_list[1][0][0]) == str(path4)
-    assert str(read_gdf.call_args_list[2][0][0]) == str(path5)
-    assert str(read_gdf.call_args_list[3][0][0]) == str(path6)
+    assert read_gdf.call_args_list[0][0][0] == path3
+    assert read_gdf.call_args_list[1][0][0] == path4
+    assert read_gdf.call_args_list[2][0][0] == path5
+    assert read_gdf.call_args_list[3][0][0] == path6
