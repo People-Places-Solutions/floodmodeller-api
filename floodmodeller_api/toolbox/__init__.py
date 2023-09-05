c7cd8a3242d3e93a2bb324676ee33d6cd7e572aa
Rees Inglis <Benjamin.ReesInglis@jacobs.com>
2023-09-05 11:25:26 +0100

    In the process of turning script into a tool that can be run from the cmd line

---
+++ floodmodeller_api/toolbox/__init__.py
@@ -0,0 +1,4 @@
+from .model_build import RaiseBedLevelsTool
+from .model_conversion import TuflowToFloodModeller
+from .visualisation import InteractiveFlowTool
+from .results_analysis import StructureLog
