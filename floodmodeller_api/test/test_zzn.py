6e3b16571539320e4f8c8fc49e1c40815d4548b7
scleakey <37449465+scleakey@users.noreply.github.com>
2024-04-18 16:49:48 +0100

    test load zzn using ief

--- floodmodeller_api/test/test_zzn.py
+++ floodmodeller_api/test/test_zzn.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pandas as pd
 import pytest
 
-from floodmodeller_api import ZZN
+from floodmodeller_api import IEF, ZZN
 
 
 @pytest.fixture
@@ -28,3 +28,9 @@ def test_load_zzn_using_dll(zzn_fp, tab_csv_output, tmpdir):
     output = pd.read_csv(Path(tmpdir, "test_output.csv"))
     output = round(output, 3)
     pd.testing.assert_frame_equal(output, tab_csv_output, rtol=0.0001)
+
+
+def test_load_zzn_using_ief(zzn_fp):
+    zzn = ZZN(zzn_fp).to_dataframe()
+    zzn_from_ief = IEF(zzn_fp.with_suffix(".ief")).get_results().to_dataframe()
+    pd.testing.assert_frame_equal(zzn, zzn_from_ief)
