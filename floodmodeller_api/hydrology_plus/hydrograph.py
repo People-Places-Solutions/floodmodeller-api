fa73f9b2c6d4ef1fb3cd7a60f4989d040cf11b43
CABALLVA <victor.caballeroponce@jacobs.com>
2024-07-26 12:10:01 +0100

    Linting undertaken

--- floodmodeller_api/hydrology_plus/hydrograph.py
+++ floodmodeller_api/hydrology_plus/hydrograph.py
@@ -45,29 +45,31 @@ class HydrographPlus(FMFile):
         self.metadata = self._get_metadata()
         self.data_flows = self._get_df_hydrographs_plus()
 
-
     def _get_metadata(self) -> dict[str, str]:
         """To extract the metada from the hydrology + results"""
-        metadata_row_index = self.data_file.index[self.data_file.apply(lambda row: row.str.contains("Return Period")).any(axis=1)][0]
+        metadata_row_index = self.data_file.index[
+            self.data_file.apply(lambda row: row.str.contains("Return Period")).any(axis=1)
+        ][0]
         metadata_df = self.data_file.iloc[1:metadata_row_index, 0:1]
 
         return {row.split("=")[0]: row.split("=")[1] for row in metadata_df.iloc[:, 0]}
 
-
     def _get_df_hydrographs_plus(self) -> pd.DataFrame:
         """To extract all the events generated in hydrology +"""
-        time_row_index = self.data_file.index[self.data_file.apply(lambda row: row.str.contains("Time \(hours\)")).any(axis=1)][0]
+        time_row_index = self.data_file.index[
+            self.data_file.apply(lambda row: row.str.contains(r"Time \(hours\)")).any(axis=1)
+        ][0]
         self.columns = self.data_file.iloc[time_row_index]
-        df_events = self.data_file.iloc[time_row_index+1:].reset_index(drop=True)
+        df_events = self.data_file.iloc[time_row_index + 1 :].reset_index(drop=True)
         df_events.columns = self.columns
         for col in df_events.columns[1:]:
             df_events[col] = pd.to_numeric(df_events[col], errors="coerce")
 
         return df_events
 
-
     def get_event(self, event: str) -> pd.DataFrame:
         """To extract a particular event to be simulated in Flood Modeller"""
+
         def _remove_string_from_list_items(lst, string) -> list[str]:
             return [item.replace(string, "") for item in lst]
 
@@ -76,6 +78,7 @@ class HydrographPlus(FMFile):
                 if string in item:
                     return i
             return -1
+
         index_columns = _remove_string_from_list_items(self.data_flows.columns, " - Flow (m3/s)")
         index_event = _find_index(index_columns, event)
         column_index = [0, index_event]
@@ -85,7 +88,9 @@ class HydrographPlus(FMFile):
 
 if __name__ == "__main__":
     event = "2020 Upper - 11 - 1"
-    baseline_unchecked = HydrographPlus(r"C:\Users\caballva\OneDrive - Jacobs\Documents\PROJECTS\FLOOD_MODELLER_API\H+\H+ForFMAPI2\associated_data\Baseline unchecked.csv")
+    baseline_unchecked = HydrographPlus(
+        r"..\floodmodeller_api\test\test_data\Baseline unchecked.csv",
+    )
     print("################################################")
     print(baseline_unchecked.metadata)
     print("################################################")
