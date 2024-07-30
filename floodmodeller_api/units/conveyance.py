4d1503f85ac9e54ca4174bd9ed8ba89d0d9c5310
scleakey <37449465+scleakey@users.noreply.github.com>
2024-07-30 16:48:21 +0100

    stack and sort

--- floodmodeller_api/units/conveyance.py
+++ floodmodeller_api/units/conveyance.py
@@ -237,12 +237,9 @@ def calculate_weighted_mannings(
 def line_to_segments(line: LineString | MultiLineString) -> np.ndarray[list]:
     """Convert a LineString or MultiLineString into a list of LineString segments."""
     if isinstance(line, LineString):
-        segments = np.array(
-            [
-                np.array(sorted([start, end], key=lambda x: x[0]))
-                for start, end in zip(line.coords[:-1], line.coords[1:])
-            ],
-        )
+        coords = np.array(line.coords)
+        unsorted_segments = np.stack((coords[:-1], coords[1:]), axis=1)
+        segments = np.sort(unsorted_segments, axis=1)
     elif isinstance(line, MultiLineString):
         segments = np.concatenate([line_to_segments(geom) for geom in line.geoms])
     else:
