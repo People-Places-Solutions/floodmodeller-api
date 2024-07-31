3394e651abe0c3d8973e5bd33c115f879b640bde
Victor Caballero <victor.caballeroponce@jacobs.com>
2024-07-31 15:29:47 +0100

    Linting

--- floodmodeller_api/test/test_hydrographplus.py
+++ floodmodeller_api/test/test_hydrographplus.py
@@ -1,4 +1,5 @@
 """Tests to check the class HydrographPlus"""
+
 import os
 from pathlib import Path
 
@@ -12,23 +13,24 @@ from floodmodeller_api.hydrology_plus.hydrograph import HydrographPlusExport
 def metada_csv():
     """To extract the metadata from the csv to be compared with the class"""
 
-    return {"Hydrograph Name":"Baseline unchecked",
-            "Hydrograph Description":"",
-            "Calculation Point":"CP_003",
-            "ReFH2 Name":"CP_003_ReFH2_1",
-            "Winfap Name":"CP_003_WINFAP_1",
-            "Urban/Rural":"Urban",
-            "Urban/Rural Comment":"",
-            "ReFH2 Comment":"",
-            "Winfap Comment":"",
-            "Winfap Distribution":"GEV",
-            "Winfap Distribution Comment":"",
-            "Use Climate Change Allowances":"True",
-            "Use Custom Scale Factors":"False",
-            "Created By":"KA007155",
-            "Created Date":"30/04/2024 09:42:23",
-            "Checksum":"ef77d9bd-2eb3-4689-a1e3-665d293db810"
-            }
+    return {
+        "Hydrograph Name": "Baseline unchecked",
+        "Hydrograph Description": "",
+        "Calculation Point": "CP_003",
+        "ReFH2 Name": "CP_003_ReFH2_1",
+        "Winfap Name": "CP_003_WINFAP_1",
+        "Urban/Rural": "Urban",
+        "Urban/Rural Comment": "",
+        "ReFH2 Comment": "",
+        "Winfap Comment": "",
+        "Winfap Distribution": "GEV",
+        "Winfap Distribution Comment": "",
+        "Use Climate Change Allowances": "True",
+        "Use Custom Scale Factors": "False",
+        "Created By": "KA007155",
+        "Created Date": "30/04/2024 09:42:23",
+        "Checksum": "ef77d9bd-2eb3-4689-a1e3-665d293db810",
+    }
 
 
 @pytest.fixture()
@@ -51,7 +53,9 @@ def event_csv():
 def hydrographplus_object():
     """To create the object to make the comparison with the csv data"""
 
-    return HydrographPlusExport(Path(os.path.dirname(__file__), r"test_data\Baseline_unchecked.csv"))
+    return HydrographPlusExport(
+        Path(os.path.dirname(__file__), r"test_data\Baseline_unchecked.csv")
+    )
 
 
 def test_data_metadata(metada_csv, hydrographplus_object):
@@ -67,6 +71,3 @@ def test_data_flows_df(datafame_csv, hydrographplus_object):
 def test_data_event(event_csv, hydrographplus_object, event="2020 Upper - 11 - 1"):
     """To compare the event between the csv and the class"""
     assert event_csv.equals(hydrographplus_object.get_event(event))
-
-
-
