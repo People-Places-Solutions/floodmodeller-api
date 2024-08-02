43c347f3e84ebb6474516db223a25d1ef8d126f3
Victor Caballero <victor.caballeroponce@jacobs.com>
2024-08-02 09:30:23 +0100

    New helper file

---
+++ floodmodeller_api/hydrology_plus/helper.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from hydrograph import HydrographPlusExport
+
+
+def load_hydrology_plus_csv_export(file: Path):   # initially, the argument was csv: Path
+
+    return HydrographPlusExport(file)
+
+
+if __name__ == "__main__":
+    load_hydrology_plus_csv_export(
+        r"..\floodmodeller-api\floodmodeller_api\test\test_data\Baseline_unchecked.csv",
+    )
