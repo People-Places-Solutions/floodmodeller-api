53d123ecc9ec2b1f6833705761e0a55e3668996d
joe-pierce <56606086+joe-pierce@users.noreply.github.com>
2024-08-15 17:07:02 +0100

    Ied comment support (#127)
    * support comments in ied
    
    * linting

--- floodmodeller_api/test/test_ied.py
+++ floodmodeller_api/test/test_ied.py
@@ -10,6 +10,11 @@ def ied_fp(test_workspace):
     return Path(test_workspace, "network.ied")
 
 
+@pytest.fixture
+def ied_fp_comments(test_workspace):
+    return Path(test_workspace, "network_with_comments.ied")
+
+
 def test_open_ied_does_not_change_file(ied_fp):
     """IED: Test str representation equal to ied file with no changes"""
     with open(ied_fp) as ied_file:
@@ -27,3 +32,10 @@ def test_open_ied_does_not_change_file(ied_fp):
         "     0.000 1.000e+09",
     ]
     assert ied.boundaries["CS26"]._write() == cs26_expected
+
+
+def test_ied_with_comments(ied_fp_comments):
+    with open(ied_fp_comments) as ied_file:
+        data_before = ied_file.read()
+    ied = IED(ied_fp_comments)
+    assert ied._write() == data_before
