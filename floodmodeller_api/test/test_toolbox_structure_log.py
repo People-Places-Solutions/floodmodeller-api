414859bd8bea771f50ea694a43c95f0fe2a3e4b2
Jacob Toller <jacob.toller@jacobs.com>
2024-11-04 09:34:47 +0000

    removed tests which were relevant pre-refactor

--- floodmodeller_api/test/test_toolbox_structure_log.py
+++ floodmodeller_api/test/test_toolbox_structure_log.py
@@ -29,6 +29,7 @@ def conduit_empty():
     c.height = 0
     c.width = 0
     c.diameter = 0
+    c.elevation_invert = 0
     c.friction_on_invert = 0
     c.friction_on_soffit = 0
     c.friction_on_walls = 0
@@ -53,6 +54,7 @@ def conduit_filled():
     c.height = 25.45
     c.width = 3
     c.diameter = 6
+    c.elevation_invert = 3
     c.friction_on_invert = 1.876
     c.friction_on_soffit = 1.34
     c.friction_on_walls = 1.8
@@ -134,131 +136,20 @@ S3LS,SPILL,,,,Elevation: 20.00 x w: 100.00,1.7,
 
 def test_conduit_data(slb, conduit_empty):
     slb._dat = DAT()
-    output = slb._conduit_data(conduit_empty)
+    output,_ = slb._conduit_data(conduit_empty)
     assert output == {
         "length": 0.0,
-        "inlet": "",
-        "outlet": "",
         "total_length": 0.0,
-    }  # currently this expects the inlet and outlet values to be empty strings, because we're not returning lists this could probably be left out now
-
-
-def test_culvert_loss_data(slb):
-    output = slb._culvert_loss_data("", "")
-    assert output == ""
-    output = slb._culvert_loss_data("TEST1", "TEST2")
-    assert output == "Ki: TEST1, Ko: TEST2"
-
-
-def test_circular_data(slb, conduit_empty, conduit_filled, no_length, with_length):
-    slb._dat = DAT()
-    output = slb._circular_data(conduit_empty, no_length)
-    assert output == ["Mannings: 0", "dia: 0.00 x l: 0.00"]
-    output = slb._circular_data(conduit_filled, with_length)
-    assert output == [
-        "Mannings: [min: 1.453345, max: 3.435]",
-        "dia: 6.00 x l: 4.97",
-    ]
-
-
-def test_sprungarch_data(slb, conduit_empty, conduit_filled, no_length, with_length):
-    output = slb._sprungarch_data(conduit_empty, no_length)
-    assert output == [
-        "Mannings: 0",
-        "(Springing: 0.00, Crown: 0.00) x w: 0.00 x l: 0.00",
-    ]
-    output = slb._sprungarch_data(conduit_filled, with_length)
-    assert output == [
-        "Mannings: [min: 1.34, max: 1.876]",
-        "(Springing: 23.10, Crown: 5.40) x w: 3.00 x l: 4.97",
-    ]
-
-
-def test_rectangular_data(slb, conduit_empty, conduit_filled, no_length, with_length):
-    output = slb._rectangular_data(conduit_empty, no_length)
-    assert output == ["Mannings: 0", "h: 0.00 x w: 0.00 x l: 0.00"]
-    output = slb._rectangular_data(conduit_filled, with_length)
-    assert output == [
-        "Mannings: [min: 1.34, max: 1.876]",
-        "h: 25.45 x w: 3.00 x l: 4.97",
-    ]
-
-
-def test_section_data(slb, conduit_empty, conduit_filled, no_length, with_length):
-    output = slb._section_data(conduit_empty, no_length)
-    assert output == ["Colebrook-White: 0", "h: 0.00 x w: 0.00 x l: 0.00"]
-    output = slb._section_data(conduit_filled, with_length)
-    assert output == [
-        "Colebrook-White: [min: 0.0, max: 4.0]",
-        "h: 65.00 x w: 150.00 x l: 4.97",
-    ]
-
-
-def test_sprung_data(slb, conduit_empty, conduit_filled, no_length, with_length):
-    output = slb._sprung_data(conduit_empty, no_length)
-    assert output == [
-        "Mannings: 0",
-        "(Springing: 0.00, Crown: 0.00) x w: 0.00 x l: 0.00",
-    ]
-    output = slb._sprung_data(conduit_filled, with_length)
-    assert output == [
-        "Mannings: [min: 1.34, max: 1.876]",
-        "(Springing: 23.10, Crown: 5.40) x w: 3.00 x l: 4.97",
-    ]
-
-
-def test_orifice_dimensions(slb, structure):
-    structure.invert = 1
-    output = slb._orifice_dimensions(structure)
-    assert output == "h: -1.00 x w: -1.00"
-
-
-def test_spill_data(slb, structure):
-    structure.data = pd.DataFrame(data={"X": [0, 0], "Y": [0, 0]})
-    structure.weir_coefficient = 0
-    output = slb._spill_data(structure)
-    assert output == ["Elevation: 0.00 x w: 0.00", 0]
-
-
-def test_bridge_data(slb, structure):
-    structure.section_data = pd.DataFrame(data={"X": [0, 0], "Y": [0, 0], "Mannings n": [0, 0]})
-    structure.opening_data = pd.DataFrame(
-        data={"Start": 0, "Finish": 0, "Springing Level": 0, "Soffit Level": 0},
-        index=[0],
-    )
-    output = slb._bridge_data(structure)
-    assert output == ["Mannings: 0", "h: 0.00 x w: 0.00"]
-
-
-def test_add_conduits(slb, conduit_filled, tmpdir):
-    # this test doesnt assert anything, and Im not sure what its trying to test.
-    slb._dat = DAT()
-    prev_c = copy.deepcopy(conduit_filled)
-    prev_c.dist_to_next = 0
-    prev_c.name = "prev"
-    slb._dat.conduits["prev"] = prev_c
-    conduit_filled.dist_to_next = 5
-    slb._dat.conduits["test_conduit"] = conduit_filled
-    next_c = copy.deepcopy(conduit_filled)
-    next_c.dist_to_next = 0
-    slb._dat.conduits["next"] = next_c
-    slb._dat._all_units = [prev_c, conduit_filled, next_c]
-    conduit_non_subtype = copy.deepcopy(conduit_filled)
-    conduit_non_subtype._subtype = "NON_SUBTYPE"
-    slb._dat.conduits["test_conduit_NON_SUBTYPE"] = conduit_non_subtype
-
-    tmp_csv = Path(tmpdir) / "temp_structure_data.csv"
-    with tmp_csv.open("w") as file:
-        slb._writer = csv.writer(file)
-        slb._add_conduits()
+    }
 
 
 def test_multi_conduits(slb, conduit_chain_dat, tmpdir):
-    expected = """first,CONDUIT,SECTION,,"Colebrook-White: [min: 0.0, max: 4.0]",h: 65.00 x w: 150.00 x l: 10.00 (Total length between first and fifth: 40.0m),,
-second,CONDUIT,SECTION,,"Colebrook-White: [min: 0.0, max: 4.0]",h: 65.00 x w: 150.00 x l: 10.00,,
-third,CONDUIT,SECTION,,"Colebrook-White: [min: 0.0, max: 4.0]",h: 65.00 x w: 150.00 x l: 10.00,,
-fourth,CONDUIT,SECTION,,"Colebrook-White: [min: 0.0, max: 4.0]",h: 65.00 x w: 150.00 x l: 10.00,,
-fifth,CONDUIT,SECTION,,"Colebrook-White: [min: 0.0, max: 4.0]",h: 65.00 x w: 150.00 x l: 0.00,,
+    expected = """Unit Name,Unit Type,Unit Subtype,Comment,Friction,Dimensions (m),Weir Coefficient,Culvert Inlet/Outlet Loss
+first,CONDUIT,SECTION,,"Colebrook-White: [min: 0.000, max: 4.000]",h: 65.00 x w: 156.00 x l: 10.00 (Total conduit length: 40.00),,
+second,CONDUIT,SECTION,,"Colebrook-White: [min: 0.000, max: 4.000]",h: 65.00 x w: 156.00 x l: 10.00,,
+third,CONDUIT,SECTION,,"Colebrook-White: [min: 0.000, max: 4.000]",h: 65.00 x w: 156.00 x l: 10.00,,
+fourth,CONDUIT,SECTION,,"Colebrook-White: [min: 0.000, max: 4.000]",h: 65.00 x w: 156.00 x l: 10.00,,
+fifth,CONDUIT,SECTION,,"Colebrook-White: [min: 0.000, max: 4.000]",h: 65.00 x w: 156.00 x l: 0.00,,
 """
 
     slb._dat = conduit_chain_dat
@@ -267,6 +158,7 @@ fifth,CONDUIT,SECTION,,"Colebrook-White: [min: 0.0, max: 4.0]",h: 65.00 x w: 150
     with tmp_csv.open("w", newline="") as file:
         slb._writer = csv.writer(file)
         slb._add_conduits()
+        slb._write_csv_output(file)
 
     with open(tmp_csv) as read_file:
         text = read_file.read()
@@ -294,46 +186,6 @@ def test_full_dat_from_commandline(slb, tmpdir, ex18_dat_path, ex18_dat_expected
     with open(tmp_csv) as read_file:
         text = read_file.read()
     assert text == ex18_dat_expected
-
-
-def test_add_structures(slb, structure, tmpdir):
-    slb._dat = DAT()
-    structure.soffit = 3
-    structure.weir_coefficient = 1
-    structure.data = pd.DataFrame(data={"X": [0, 0], "Y": [0, 0]})
-    structure.section_data = pd.DataFrame(data={"X": [0, 0], "Y": [0, 0], "Mannings n": [0, 0]})
-    structure.opening_data = pd.DataFrame(
-        data={"Start": 0, "Finish": 0, "Springing Level": 0, "Soffit Level": 0},
-        index=[0],
-    )
-    structure.crest_elevation = 1
-    structure.weir_breadth = 1
-    structure.weir_length = 1
-    structure.weir_elevation = 1
-    slb._dat.structures["test_structure_orifice"] = structure
-    struc_spill = copy.deepcopy(structure)
-    struc_spill._unit = "SPILL"
-    slb._dat.structures["test_structure_spill"] = struc_spill
-    struc_sluice = copy.deepcopy(structure)
-    struc_sluice._unit = "SLUICE"
-    slb._dat.structures["test_structure_sluice"] = struc_sluice
-    struc_rnweir = copy.deepcopy(structure)
-    struc_rnweir._unit = "RNWEIR"
-    slb._dat.structures["test_structure_rnweir"] = struc_rnweir
-    struc_weir = copy.deepcopy(structure)
-    struc_weir._unit = "WEIR"
-    slb._dat.structures["test_structure_weir"] = struc_weir
-    struc_bridge = copy.deepcopy(structure)
-    struc_bridge._unit = "BRIDGE"
-    slb._dat.structures["test_structure_bridge"] = struc_bridge
-    struc_none = copy.deepcopy(structure)
-    struc_none._unit = "NONE"
-    slb._dat.structures["test_structure_none"] = struc_none
-
-    tmp_csv = Path(tmpdir) / "temp_structure_data.csv"
-    with tmp_csv.open("w") as file:
-        slb._writer = csv.writer(file)
-        slb._add_structures()
 
 
 if __name__ == "__main__":
