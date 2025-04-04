da3628d7d72d16fcee076f61ad503b404e76f905
joe-pierce <56606086+joe-pierce@users.noreply.github.com>
2025-04-04 10:14:15 +0000

    add test for  remove unit name

--- floodmodeller_api/test/test_unit.py
+++ floodmodeller_api/test/test_unit.py
@@ -28,3 +28,28 @@ def test_get_revision_and_comment(
     revision, comment = dummy_unit._get_revision_and_comment(header)
     assert revision == expected_revision
     assert comment == expected_comment
+
+
+@pytest.mark.parametrize(
+    ("unit", "header", "remove_revision", "expected_result"),
+    [
+        ("RESERVOIR", "RESERVOIR 45678 This is a comment", True, "45678 This is a comment"),
+        ("RESERVOIR", "RESERVOIR #revision#1 Mr Comment123", True, "Mr Comment123"),
+        (
+            "LATERAL",
+            "LATERAL #revision#1 another #revision#1 tag",
+            True,
+            "another #revision#1 tag",
+        ),
+        (
+            "LATERAL",
+            "LATERAL #revision#1 another #revision#1 tag",
+            False,
+            "#revision#1 another #revision#1 tag",
+        ),
+    ],
+)
+def test_remove_unit_name(unit: str, header: str, remove_revision: bool, expected_result: str):
+    dummy_unit = DummyUnit(unit)
+    result = dummy_unit._remove_unit_name(header, remove_revision=remove_revision)
+    assert result == expected_result
