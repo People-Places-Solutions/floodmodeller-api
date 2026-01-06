92960f612f77fea9b752157c4cb0c2f5059e4586
Jacob Toller <119618123+jacob-toller@users.noreply.github.com>
2026-01-06 16:50:08 +0000

    removed comment

--- sample_code/location_example.py
+++ sample_code/location_example.py
@@ -1,7 +1,6 @@
 from floodmodeller_api import DAT
 
 dat = DAT("floodmodeller_api/test/test_data/River_Bridge_no_gxy.dat")
-# check if unit is updated when change in allunits vs group
 
 
 for label in ("M029", "M030", "M031"):
