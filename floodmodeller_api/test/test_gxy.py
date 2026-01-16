7c9d883c6fd450d3e85e71f212382b9b43b59ddb
Shannon Leakey <37449465+scleakey@users.noreply.github.com>
2026-01-16 08:47:12 +0000

    formatting

--- floodmodeller_api/test/test_gxy.py
+++ floodmodeller_api/test/test_gxy.py
@@ -54,10 +54,8 @@ def get_supported_unit_classes():
     all_unit_classes = []
     for unit_type, attributes in floodmodeller_api.units.SUPPORTED_UNIT_TYPES.items():
         if attributes["group"] not in ("other", "comments"):
-            unit_type_safe = unit_type.replace(" ", "_").replace(
-                "-",
-                "_",
-            )  # Borrowed this from .dat
+            unit_type_safe = unit_type.replace(" ", "_").replace("-", "_")
+            # Borrowed replacing idea from .dat
             unit_class = getattr(floodmodeller_api.units, unit_type_safe)
             all_unit_classes.append(unit_class)
     return all_unit_classes
