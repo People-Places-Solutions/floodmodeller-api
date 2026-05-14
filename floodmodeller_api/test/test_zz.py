79804b7c0cd9706c98ab6d4001a3d7a94f054bf1
Jacob Toller <119618123+jacob-toller@users.noreply.github.com>
2026-05-14 11:10:03 +0000

    linting

--- floodmodeller_api/test/test_zz.py
+++ floodmodeller_api/test/test_zz.py
@@ -66,7 +66,6 @@ def test_max(zzn: ZZN, zzx: ZZX, folder: Path, csv: str, file: str):
         ("Left FP mode", "network_zzx_left_fp_mode.csv", "zzx"),
     ],
 )
-# TODO: add negative results to this?
 def test_all_timesteps(zzn: ZZN, zzx: ZZX, folder: Path, variable: str, csv: str, file: str):
     """
     This test compares data extracted from ZZN/ZZX files against known values from .csv.
@@ -160,31 +159,22 @@ def test_zz_reads_correct_start_end(test_workspace, simulation_case, file_type):
     zz_filepath = test_workspace / simulation_case / f"{simulation_case.upper()}.{file_type}"
     zz: ZZN | ZZX = read_file(zz_filepath)
     zz_df = zz.to_dataframe()
-    pass
 
     assert zz_df.index[0] == ief.start
     assert zz_df.index[-1] == ief.finish
 
     # We expect saving to happen on both the first and last timestep, hence the `+1`
-    expected_rows = ((ief.finish-ief.start) * (3600/ief.saveinterval)) + 1
+    expected_rows = ((ief.finish - ief.start) * (3600 / ief.saveinterval)) + 1
     assert len(zz_df.index) == expected_rows
-
 
     # Check the `result_type="max"` case and compare against the main df
     # In cases where this would produce a false positive, the previous assertions should fail.
-    zz_max_df = zz.to_dataframe(result_type="max",include_time=True)
-    units_to_check = ["M040","M042","M054"]
+    zz_max_df = zz.to_dataframe(result_type="max", include_time=True)
+    units_to_check = ["M040", "M042", "M054"]
     variable = "Flow" if file_type == "zzn" else "Link inflow"
 
     for label in units_to_check:
         max_flow = zz_df[variable][label].max()
         max_flow_time = zz_df[variable][label].idxmax()
-        pass
-        assert round(zz_max_df.loc[label][f"Max {variable}"],3) == pytest.approx(max_flow,0.001)
+        assert round(zz_max_df.loc[label][f"Max {variable}"], 3) == pytest.approx(max_flow, 0.001)
         assert zz_max_df.loc[label][f"Max {variable} Time(hrs)"] == max_flow_time
-        
-
-
-
-
-
